055a6ac405f474040b582f61461512d16d12218d
Johanes Gregorius Indra Pradana Pao <me@johanespao.dev>
2025-10-24 18:08:34 +0700

    ambil_data in main.py need 1 additional parameter for action_id

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from rahasia import Rahasia
 from endpoint.rekues import Request
-from proses import all_products
+from endpoint.enums import ActionId
+from proses import ambil_data
 
 if __name__ == "__main__":
     rahasia = Rahasia()
     rekues = Request(rahasia.param.url, rahasia.param.id_perusahaan)
-    proses = all_products(rekues, limit=200)
+    proses = ambil_data(rekues, ActionId.GET_ALL_PRODUCTS, limit=200)
     print(proses)
