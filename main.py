de461a2bf593aecaa9feb387de37a1bd8b514c75
Johanes Gregorius Indra Pradana Pao <me@johanespao.dev>
2025-10-27 18:13:26 +0700

    Testing master_produk_gap_gpa()

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from rahasia import Rahasia
 from endpoint.rekues import Request
 from endpoint.enums import ActionId
 from proses import ambil_data
+from pemrosesan_data.produk import master_produk_gap_gpa
 
 if __name__ == "__main__":
     rahasia = Rahasia()
     rekues = Request(rahasia.param.url, rahasia.param.id_perusahaan)
-    proses = ambil_data(rekues, ActionId.GET_ALL_PRODUCTS, limit=200)
-    print(proses)
+    gap = ambil_data(rekues, ActionId.GET_ALL_PRODUCTS, 200)
+    gpa = ambil_data(rekues, ActionId.GET_PRODUCT_ALIAS, 200)
+
+    master_produk_gap_gpa(gap, gpa)
